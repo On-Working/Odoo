ba4dcdb37503907bf3adffb802b6884f557675db
David Martinez <dgorp.me@gmail.com>
2023-10-06 10:52:17 -0500

    ODOO-12-FTP_Connection_and_products_creation

--- ct/ct_call.py
+++ ct/ct_call.py
@@ -1,12 +1,44 @@
 from decouple import config
 from ftplib import FTP
+import json
 import requests
 
 url = config("ct_url", default="")  # * Url del API
 
 
 def ct_catalogue():
-    print("CT Catalogo")
+    print("Iniciando conexión FTP con CT")
+
+    # ? Datos ftp
+    ftp_host = config("ct_host", default="")
+    ftp_user = config("ct_user", default="")
+    ftp_password = config("ct_pass", default="")
+    ftp_directory = config("ct_dir", default="")
+    json_file_name = config("ct_file", default="")
+
+    # ? Instancia FTP
+    ftp = FTP(ftp_host)
+
+    # ? Inicio de sesion FTP
+    ftp.login(ftp_user, ftp_password)
+
+    # ? Directorio deseado
+    ftp.cwd(ftp_directory)
+
+    # ? Archivo JSON
+    with open(json_file_name, "wb") as local_file:
+        ftp.retrbinary("RETR " + json_file_name, local_file.write)
+
+    # ? Conexion FTP cerrada
+    ftp.quit()
+
+    # ? Carga y trabaja con el archivo JSON localmente
+    with open(json_file_name, "r") as local_file:
+        productos = json.load(local_file)
+
+    print("Conexión con el FTP de CT exitosa")
+    print("Retornando datos: Info, Data \n")
+    return productos
 
 
 def ct_stock():
