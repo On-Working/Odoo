6b0feb28dbc10962bb6fa9a20762186f1d08c325
David Martinez <dgorp.me@gmail.com>
2023-11-27 16:17:48 -0500

    ODOO-22-Odoo_ecommerce_v0.2_Beta

--- main.py
+++ main.py
@@ -14,36 +14,36 @@ TEC = "Tec"
 
 
 def main():
-    # odoo = netdata.odoo_sys_uno()
+    odoo = netdata.odoo_sys_uno()
 
-    # local_time = time.strftime(f"{TIME_STR} \n")
-    # print(START.format(SYS, local_time), end="\r")
-    # print("")
-    # sys_products = catalogue.sys_main(odoo)
-    # sys_success = sys_products[0]
-    # sys_fails = sys_products[1]
-    # print(
-    #     RESULT.format(sys_success, sys_fails),
-    #     end="\r",
-    # )
-    # print("")
-    # local_time = time.strftime(f"{TIME_STR} \n\n")
-    # print(END.format(SYS, local_time), end="\r")
+    local_time = time.strftime(f"{TIME_STR} \n")
+    print(START.format(SYS, local_time), end="\r")
+    print("")
+    sys_products = catalogue.sys_main(odoo)
+    sys_success = sys_products[0]
+    sys_fails = sys_products[1]
+    print(
+        RESULT.format(sys_success, sys_fails),
+        end="\r",
+    )
+    print("")
+    local_time = time.strftime(f"{TIME_STR} \n\n")
+    print(END.format(SYS, local_time), end="\r")
 
-    # odoo = netdata.odoo_ct_uno()
-    # local_time = time.strftime(f"{TIME_STR} \n")
-    # print(START.format(CT, local_time), end="\r")
-    # print("")
-    # ct_products = catalogue.ct_main(odoo)
-    # ct_success = ct_products[0]
-    # ct_fails = ct_products[1]
-    # print(
-    #     RESULT.format(ct_success, ct_fails),
-    #     end="\r",
-    # )
-    # print("")
-    # local_time = time.strftime(f"{TIME_STR} \n\n")
-    # print(END.format(CT, local_time), end="\r")
+    odoo = netdata.odoo_ct_uno()
+    local_time = time.strftime(f"{TIME_STR} \n")
+    print(START.format(CT, local_time), end="\r")
+    print("")
+    ct_products = catalogue.ct_main(odoo)
+    ct_success = ct_products[0]
+    ct_fails = ct_products[1]
+    print(
+        RESULT.format(ct_success, ct_fails),
+        end="\r",
+    )
+    print("")
+    local_time = time.strftime(f"{TIME_STR} \n\n")
+    print(END.format(CT, local_time), end="\r")
 
     odoo = netdata.odoo_tec_uno()
     local_time = time.strftime(f"{TIME_STR} \n")
